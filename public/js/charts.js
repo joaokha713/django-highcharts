70a49814a19cea4b1e7de815253637cdd691bd0a
Joao Silva <joaokha713@gmail.com>
2017-06-25 17:20:26 +0100

    feat(charts): added legends to piechart

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,5 +1,5 @@
 (function ($, palette) {
-    function setChart2(data) {
+    function setChart2(labels, data) {
         var canvas = document.getElementById("myChart");
         var ctx = canvas.getContext("2d");
         var lastend = 0;
@@ -10,26 +10,52 @@
             myTotal += data[e];
         }
 
+        // Draw Arcs
+        var width = canvas.width;
+        var height = canvas.height;
+        var centerX = width / 2;
+        var centerY = height / 2;
+        var radius = Math.min(width / 2 - 100, height / 2 - 100);
+        var outerRadius = radius + 50;
+
         for (var i = 0; i < data.length; i++) {
             ctx.fillStyle = '#' + myPalette[i];
             ctx.beginPath();
-            ctx.moveTo(canvas.width / 2, canvas.height / 2);
-            ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 2, lastend, lastend + (Math.PI * 2 * (data[i] / myTotal)), false);
-            ctx.lineTo(canvas.width / 2, canvas.height / 2);
+            ctx.moveTo(centerX, centerY);
+            ctx.arc(centerX, centerY, radius, lastend, lastend + (Math.PI * 2 * (data[i] / myTotal)), false);
             ctx.fill();
             lastend += Math.PI * 2 * (data[i] / myTotal);
+        }
+
+        // Draw Labels
+        lastend = 0;
+        for (var i = 0; i < data.length; i++) {
+            var radian = lastend + Math.PI * 2 * (data[i] / myTotal / 2);
+            var sin = Math.sin(-(radian - Math.PI / 2));
+            var cos = Math.cos(radian - Math.PI / 2);
+            var x = centerX + sin * outerRadius;
+            var y = centerY + cos * outerRadius;
+
+            ctx.fillStyle = '#' + myPalette[i];
+            ctx.textAlign = 'center';
+            ctx.fillText(labels[i], x, y);
+            lastend += Math.PI * 2 * (data[i] / myTotal);
+
+            // draw line to legend
+            ctx.beginPath();
+            ctx.moveTo(centerX + sin * radius, centerY + cos * radius);
+            ctx.lineTo(centerX + sin * (radius + 30), centerY + cos * (radius + 30));
+            ctx.stroke();
         }
     }
 
     var endpoint = '/api/chart/data/';
     var defaultData = [];
-    var labels = [];
     $.ajax({
         method: "GET",
         url: endpoint,
         success: function (data) {
-            labels = data.labels;
-            setChart2(data.values);
+            setChart2(data.labels, data.values);
         },
         error: function (error_data) {
             console.log("error");
